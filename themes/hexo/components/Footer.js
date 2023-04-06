388741d400f677325b1962e256e91449d70a69c2
SingshunWong <128598979+SingshunWong@users.noreply.github.com>
2023-04-06 14:59:39 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -17,8 +17,6 @@ const Footer = ({ title }) => {
       className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
     >
       {/* <DarkModeButton/> */}
-
-      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
       
         <span className='text-xs '>Powered by <a href='https://hxs.wiki'className='underline dark:text-gray-300'>SingShun </a>.</span></span><br/>
 
