fa75c5c88adaff3d971f3252c254bd9b93ece6b4
SingshunWong <128598979+SingshunWong@users.noreply.github.com>
2023-04-06 15:06:19 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -17,8 +17,16 @@ const Footer = ({ title }) => {
       className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
     >
       {/* <DarkModeButton/> */}
-      
-        <span className='text-xs '>Powered by <a href='https://hxs.wiki'className='underline dark:text-gray-300'>SingShun </a>.</span></span><br/>
+
+      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
+
+      <span className='hidden busuanzi_container_site_pv'>
+            <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
+      <span className='pl-2 hidden busuanzi_container_site_uv'>
+        <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
+        <br/>
+        
+        <span className='text-xs '>Powered by <a href='https://hsx.wiki' className='underline dark:text-gray-300'>SingShun</a>.</span></span><br/>
 
     </footer>
   )
