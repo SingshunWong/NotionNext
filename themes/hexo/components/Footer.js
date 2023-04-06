49d2dfaf01fc0df18505a57ef2cab1b1be551b8c
SingshunWong <128598979+SingshunWong@users.noreply.github.com>
2023-04-06 14:45:47 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -18,15 +18,10 @@ const Footer = ({ title }) => {
     >
       {/* <DarkModeButton/> */}
 
-      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<br/>
+      <i className='fas fa-copyright' /> {`${copyrightDate}`} <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<br/>
 
       {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
-
-      <span className='hidden busuanzi_container_site_pv'>
-            <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
-      <span className='pl-2 hidden busuanzi_container_site_uv'>
-        <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-        <br/>
+      <br/>
         <span className='text-xs '>Powered by <a href='https://hxs.wiki'className='underline dark:text-gray-300'>SingShun {BLOG.VERSION}</a>.</span></span><br/>
 
     </footer>
